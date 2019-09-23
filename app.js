1462b219a9551bcb2078d61fb7a071f4e7087390
Agustin Francesconi <agustinfrancesconi@gmail.com>
2019-09-23 09:48:23 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+ 
+var app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/detail', function (req, res) {
+    res.render('detail', req.query);
+});
+
+app.use(express.static('assets'));
+ 
+app.use('/assets', express.static(__dirname + '/assets'));
+ 
+app.listen(3000);
